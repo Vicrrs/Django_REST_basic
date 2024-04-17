5b5d24e00794b0b401daa73d20b96b052036b81e
Vicrrs <enricofermi2019@gmail.com>
2024-04-17 02:38:36 -0400

    comentario sobre extra_kargs

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -5,8 +5,8 @@ from .models import Avaliacao
 class AvaliacaoSerializer(serializers.ModelSerializer):
     class Meta:
         extra_kargs = {
-            'email': {'write_only': True}
-        }
+            'email': {'write_only': True}  # email nao vai ser apresentado
+        }  # email exigido apenas para cadastro, por questao de seguranca
         model = Avaliacao
         fields = (
             'id',
